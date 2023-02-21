0b6aa9a667621c90cae868de8b44b33ad42a9ea9
Syd <smpospech@yahoo.com>
2023-02-20 23:24:47 -0600

    SYD CHANGES WOOHOO

--- MovementDemo - September_27/Assets/V2 Scripts/LevelSelect.cs
+++ MovementDemo - September_27/Assets/V2 Scripts/LevelSelect.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+    public void loadLevel()
+    {
+        SceneManager.LoadScene("CF_1");
+    }
     public void quitGame()
     {
         Application.Quit();
