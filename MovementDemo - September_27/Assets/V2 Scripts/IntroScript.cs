e68473cca04df6b30aa8a68a8a2f62b465893b84
Syd <smpospech@yahoo.com>
2023-05-04 02:11:23 -0500

    Syd did stuff

--- MovementDemo - September_27/Assets/V2 Scripts/IntroScript.cs
+++ MovementDemo - September_27/Assets/V2 Scripts/IntroScript.cs
@@ -8,5 +8,6 @@ public class IntroScript : MonoBehaviour
     private void OnEnable()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("You switched scenes");
     }
 }
